0987c56fac7cb006f88382e110ea17b3dd2c43f2
buckldav <runalldayandnight@gmail.com>
2018-12-24 20:46:38 -0700

    PostgresSQL Database

--- reactdjango/settings.py
+++ reactdjango/settings.py
@@ -84,12 +84,24 @@ WSGI_APPLICATION = 'reactdjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+  DATABASES = {
+    'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
   }
-}
+else:
+  DATABASES = {
+    'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'd99e14q4fetdir',
+      'USER': os.environ["DATABASE_USER"],
+      'PASSWORD': os.environ["DATABASE_PASSWORD"],
+      'HOST': 'ec2-107-20-237-78.compute-1.amazonaws.com',
+      'PORT': '5432',
+    }
+  }
 
 
 # Password validation
