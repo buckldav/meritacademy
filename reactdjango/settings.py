3c63237cf25553e008e0618c27c2038b852550a2
buckldav <runalldayandnight@gmail.com>
2018-12-24 21:15:55 -0700

    Database

--- reactdjango/settings.py
+++ reactdjango/settings.py
@@ -93,7 +93,9 @@ if DEBUG:
   }
 else:
   import dj_database_url
-  DATABASES['default'] = dj_database_url.config(os.environ['DATABASE_URL'])
+  DATABASES = {
+    'default': dj_database_url.config(os.environ['DATABASE_URL'])
+  }
 
 
 # Password validation
